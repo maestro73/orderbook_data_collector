5070ae84b512288c6e07a663b59bb811b0aeeb47
xiaomaniay <xiaomaniay@gmail.com>
2020-04-20 18:06:59 +0800

    data collector

---
+++ data_collector.py
@@ -0,0 +1,80 @@
+import websocket
+import zlib
+import json
+import threading
+import time
+
+import redis_upload
+
+# decompress the received data
+def inflate(data):
+    decompress = zlib.decompressobj(-zlib.MAX_WBITS)
+    inflated = decompress.decompress(data)
+    inflated += decompress.flush()
+    return inflated
+
+
+# check the received message
+def on_message(ws, message):
+    try:
+        inflated = inflate(message).decode('utf-8')
+        if inflated == '{"event":"pong"}':
+            print("Pong received.")
+            return
+        global order_book
+        msg = json.loads(inflated)
+        if "channel" in msg and 'spot/depth5:ETH-USDT' == msg["channel"]:  # check whether the correct channel is subscribed
+            print(msg["channel"] + " subscribed.")
+        if "table" in msg and 'spot/depth5' == msg["table"]:  # check the data type
+            for data in msg["data"]:
+                order_book = data
+                print(order_book)
+                redis_upload.upload('spot/depth5:ETH-USDT', str(order_book))
+    except Exception as e:
+        print(e)
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws):
+    print("### closed ###")
+
+
+# send request when connection is started
+def on_open(ws):
+    ws.send('{"op": "subscribe", "args": ["spot/depth5:ETH-USDT"]}')
+
+
+# send heartbeat to maintain the connection
+def sendHeartBeat(ws):
+    ping = '{"event":"ping"}'
+    while(True):
+        time.sleep(30) # send request every 30 seconds
+        sent = False
+        while(sent is False): # keep trying if failed
+            try:
+                ws.send(ping)
+                sent = True
+                print("Ping sent.")
+            except Exception as e:
+                print(e)
+
+
+# create websocket connection
+def ws_main():
+    websocket.enableTrace(True)
+    host = "wss://real.OKEx.com:8443/ws/v3"
+    ws = websocket.WebSocketApp(host,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    threading.Thread(target=sendHeartBeat, args=(ws,)).start()
+    ws.run_forever()
+
+
+if __name__ == "__main__":
+    order_book = 0
+    threading.Thread(target=ws_main).start()
---
+++ redis_upload.py
@@ -0,0 +1,19 @@
+import redis
+
+
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
+
+
+def upload(key, value):
+    # creates a connection to Redis
+    r = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_password)
+
+    try:
+        r.append(key, value) if r.exists(key) else r.set(key, value)
+    except Exception as e:
+        print(e)
